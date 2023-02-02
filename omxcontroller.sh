78ce611b58681bfc862de9844c0d793a0d850b94
Lorcàn Petit <petitcreative345@gmail.com>
2023-02-02 16:07:04 +0100

    stp fonctionne

--- omxcontroller.sh
+++ omxcontroller.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-
 # Définissez le chemin vers vos vidéos
 video1="video/video1.mp4"
 video2="video/video2.mp4"
 video3="video/video3.mp4"
 video4="video/video4.mp4"
-
 # Boucle infinie pour lire les entrées de boutons
 while true; do
   read -r event < /dev/input/event4
